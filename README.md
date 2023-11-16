6ef292af2fa1762530e1e91ef1b1f05d111d5290
glassoctopus <glassoctopus@yahoo.com>
2023-11-15 22:17:53 -0600

    mercury hacky orbit, working, stars need some js refactoring

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # solarSystem
 
 ~attribution~
-// background - https://dev.to/0shuvo0/pure-css-infinite-spacegalaxy-with-just-one-div-4o02
+main body of content from this tutorial : https://www.youtube.com/watch?v=dlLGVHV_9Zk
+&
+https://www.youtube.com/watch?v=_xug0Qw-P_Q&list=RDCMUCeNYDojo4nU2sbHz1sMsBXw&start_radio=1&rv=_xug0Qw-P_Q&t=195
+
+star background - https://dev.to/0shuvo0/pure-css-infinite-spacegalaxy-with-just-one-div-4o02
--- main.js
+++ main.js
@@ -15,3 +15,42 @@ for(let i = 0; i < STAR_COUNT; i++){
 };
 // console.log(result.substring(0, result.length - 1));
 // outPut.innerHTML = result;
+
+const sunX = window.innerWidth / 2 - 100;
+const sunY = window.innerHeight / 2 - 100;
+
+const sun = document.querySelector('.sun');
+sun.style.left = `${sunX}px`;
+sun.style.top = `${sunY}px`;
+
+const mercury = {
+  speed: 0.005,
+  theta: 0,
+  radius: 133,
+  A: 1.75, //width of elipse
+  B: .69, // height of elipse
+  el: document.querySelector('.mercury'),
+}
+
+function update(planet) {
+  planet.theta += planet.speed;
+  planet.el.style.left = `${planet.A * Math.cos(planet.theta) * planet.radius + sunX + 100}px`;
+  // console.log("left = ", planet.el.style.left);
+  // console.log("offset X = ", planet.radius + sunX + 100);
+  // console.log("difference X = ",(planet.radius + sunX + 100) - (planet.B * Math.sin(planet.theta) * planet.radius + sunY + 100));
+  planet.el.style.top = `${planet.B * Math.sin(planet.theta) * planet.radius + sunY + 100}px`;
+  // console.log(planet.B * Math.sin(planet.theta) * planet.radius + sunY + 100 > 420);
+  if((planet.B * Math.sin(planet.theta) * planet.radius + sunY + 100) > 420){
+    planet.el.style.zIndex = 1;
+  }else{
+    planet.el.style.zIndex = -1;
+  }
+  // console.log("top = ", planet.el.style.top);
+  // console.log("offset Y = ", planet.radius + sunY + 100);
+  // console.log("difference Y = ", (planet.radius + sunY + 100) - (planet.B * Math.sin(planet.theta) * planet.radius + sunY + 100));
+  
+}
+
+setInterval(() => {
+  update(mercury);
+}, 10);
